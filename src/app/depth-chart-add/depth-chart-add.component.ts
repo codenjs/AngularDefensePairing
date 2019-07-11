01cd1437d1a04679bd2b4c020c3e79fbcfb31e13
NJS <24832578+codenjs@users.noreply.github.com>
2019-07-11 16:52:41 -0400

    Validate new names when adding to depth chart

--- src/app/depth-chart-add/depth-chart-add.component.ts
+++ src/app/depth-chart-add/depth-chart-add.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn } from '@angular/forms';
+import { DepthChartListItem } from '../depth-chart-list-item';
 
 @Component({
   selector: 'app-depth-chart-add',
@@ -7,18 +9,43 @@ import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '
 })
 export class DepthChartAddComponent implements OnInit {
 
-  newName: string;
+  addForm: FormGroup;
   @ViewChild('newNameInput', {static: false}) newNameElement: ElementRef;
+  @Input() items: DepthChartListItem[];
   @Output() nameSaved = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      newName: ['', this.duplicateNameValidator()]
+    }, {
+      updateOn: 'submit'
+    });
+  }
+  
+  // convenience getter for easy access to form field
+  get newName() { return this.addForm.get('newName'); }
+
+  duplicateNameValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      return this.items.find(i => i.name == control.value.trim()) ? {'duplicateName': {value: control.value + ' already exists'}} : null;
+    };
   }
 
-  save(): void {
-    this.nameSaved.emit(this.newName);
-    this.newName = '';
+  onSubmit(): void {
+    if (this.addForm.invalid) {
+      return;
+    }
+
+    let newName = this.newName.value.trim();
+
+    if (!newName) {
+      return;
+    }
+
+    this.nameSaved.emit(newName);
+    this.newName.setValue('');
     this.newNameElement.nativeElement.focus();
   }
 }
